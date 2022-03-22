0576e4199e53b56feafdc92ea4130aed2c5790d0
Abdyrahmanov Timurlan <timurlanabdyrahmanov7@gmail.com>
2022-03-22 15:22:49 +0600

    update project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Layout from "./Component/Layout";
+import {Route, Routes} from "react-router-dom";
+import News from "./View/News";
 
 const App = () => {
   return (
-      <div>
-        app
-      </div>
+     <div className="container">
+         <div className="app">
+             <Layout>
+                 <div className="pages">
+                     <Routes>
+                         <Route path='/news' elements={<News/>}/>
+                     </Routes>
+                 </div>
+             </Layout>
+         </div>
+     </div>
   );
 };
 
--- src/Component/Header/index.js
+++ src/Component/Header/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Header = () => {
     return (
         <div>
-            header
+           <div className='nav'>
+               <div className="nav__menu">
+                <Link className="nav__link" to='/about'>О нас</Link>
+                <Link className="nav__link" to='/collections'>Коллекции</Link>
+                <Link className="nav__link" to='/news'>Новости</Link>
+               </div>
+               <p className="header-contact">Тел: <span>+996 000 00 00 00</span></p>
+           </div>
         </div>
     );
 };
