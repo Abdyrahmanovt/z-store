7a0f7e35a2de92e90b12142b0e442cb59d58e6b6
Abdyrahmanov Timurlan <timurlanabdyrahmanov7@gmail.com>
2022-04-22 14:32:54 +0600

    news

--- src/View/News/index.js
+++ src/View/News/index.js
@@ -1,124 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './style.css'
-import newsImg from './newsImg.png'
+import axios from "axios";
 
 const News = () => {
+    const [news, setNews] = useState([])
+    useEffect(() => {
+        axios('https://613fef235cb9280017a110a6.mockapi.io/zzeon')
+            .then((data) => {
+                setNews(data.data[0].news)
+                console.log(data.data[0].news)
+            })
+    }, [])
+
+
     return (
         <div className="container">
             <div className='news'>
                 <h3 className="news__title">
                     Новости
                 </h3>
-                <div className="news__box">
-                    <img src={newsImg} alt="" className="news__img"/>
-                    <div className="news__about">
-                        <p className="news__subtitle">
-                            Lorem ipsum dolor sit amet
-                        </p>
-                        <p className="news__desc">
-                            Sit ullamcorper at gravida quis feugiat. Laoreet leo dolor, dui eget sit viverra justo,
-                            malesuada. Viverra pharetra, augue neque felis enim dui id cum. At pellentesque diam nulla
-                            ac amet quisque quis. Est consectetur ullamcorper curabitur quis viverra hac molestie. Elit
-                            pulvinar congue ut amet adipiscing felis tincidunt. Amet quis varius aliquam hendrerit
-                            tempus. Sed sit diam quis scelerisque congu econgu econgu econguecongu econguecon guecon
-                            guecon guecong ueconguecong uec ongue.
-                            Sit ullamcorper at gravida quis feugiat. Laoreet leo dolor, dui eget sit viverra justo,
-                            malesuada. Viverra pharetra, augue neque Sit ullamcorper at gravida quis feugiat. Laoreet
-                            leo dolor, dui eget sit viverra justo, malesuada. Viverra pharetra, augue neque felis enim
-                            dui id cum. At pellentesque diam nulla ac amet quisque quis. Est consectetur ullamcorper
-                            curabitur quis viverra hac molestie. Elit pulvinar congue ut amet adipiscing felis
-                            tincidunt. Amet quis varius aliquam hendrerit tempus. Sed sit diam quis
-                        </p>
-                    </div>
-                </div>
-                <div className="news__box">
-                    <img src={newsImg} alt="" className="news__img"/>
-                    <div className="news__about">
-                        <p className="news__subtitle">
-                            Lorem ipsum dolor sit amet
-                        </p>
-                        <p className="news__desc">
-                            Sit ullamcorper at gravida quis feugiat. Laoreet leo dolor, dui eget sit viverra justo,
-                            malesuada. Viverra pharetra, augue neque felis enim dui id cum. At pellentesque diam nulla
-                            ac amet quisque quis. Est consectetur ullamcorper curabitur quis viverra hac molestie. Elit
-                            pulvinar congue ut amet adipiscing felis tincidunt. Amet quis varius aliquam hendrerit
-                            tempus. Sed sit diam quis scelerisque congu econgu econgu econguecongu econguecon guecon
-                            guecon guecong ueconguecong uec ongue.
-                            Sit ullamcorper at gravida quis feugiat. Laoreet leo dolor, dui eget sit viverra justo,
-                            malesuada. Viverra pharetra, augue neque Sit ullamcorper at gravida quis feugiat. Laoreet
-                            leo dolor, dui eget sit viverra justo, malesuada. Viverra pharetra, augue neque felis enim
-                            dui id cum. At pellentesque diam nulla ac amet quisque quis. Est consectetur ullamcorper
-                            curabitur quis viverra hac molestie. Elit pulvinar congue ut amet adipiscing felis
-                            tincidunt. Amet quis varius aliquam hendrerit tempus. Sed sit diam quis
-                        </p>
-                    </div>
-                </div>
-                <div className="news__box">
-                    <img src={newsImg} alt="" className="news__img"/>
-                    <div className="news__about">
-                        <p className="news__subtitle">
-                            Lorem ipsum dolor sit amet
-                        </p>
-                        <p className="news__desc">
-                            Sit ullamcorper at gravida quis feugiat. Laoreet leo dolor, dui eget sit viverra justo,
-                            malesuada. Viverra pharetra, augue neque felis enim dui id cum. At pellentesque diam nulla
-                            ac amet quisque quis. Est consectetur ullamcorper curabitur quis viverra hac molestie. Elit
-                            pulvinar congue ut amet adipiscing felis tincidunt. Amet quis varius aliquam hendrerit
-                            tempus. Sed sit diam quis scelerisque congu econgu econgu econguecongu econguecon guecon
-                            guecon guecong ueconguecong uec ongue.
-                            Sit ullamcorper at gravida quis feugiat. Laoreet leo dolor, dui eget sit viverra justo,
-                            malesuada. Viverra pharetra, augue neque Sit ullamcorper at gravida quis feugiat. Laoreet
-                            leo dolor, dui eget sit viverra justo, malesuada. Viverra pharetra, augue neque felis enim
-                            dui id cum. At pellentesque diam nulla ac amet quisque quis. Est consectetur ullamcorper
-                            curabitur quis viverra hac molestie. Elit pulvinar congue ut amet adipiscing felis
-                            tincidunt. Amet quis varius aliquam hendrerit tempus. Sed sit diam quis
-                        </p>
-                    </div>
-                </div>
-                <div className="news__box">
-                    <img src={newsImg} alt="" className="news__img"/>
-                    <div className="news__about">
-                        <p className="news__subtitle">
-                            Lorem ipsum dolor sit amet
-                        </p>
-                        <p className="news__desc">
-                            Sit ullamcorper at gravida quis feugiat. Laoreet leo dolor, dui eget sit viverra justo,
-                            malesuada. Viverra pharetra, augue neque felis enim dui id cum. At pellentesque diam nulla
-                            ac amet quisque quis. Est consectetur ullamcorper curabitur quis viverra hac molestie. Elit
-                            pulvinar congue ut amet adipiscing felis tincidunt. Amet quis varius aliquam hendrerit
-                            tempus. Sed sit diam quis scelerisque congu econgu econgu econguecongu econguecon guecon
-                            guecon guecong ueconguecong uec ongue.
-                            Sit ullamcorper at gravida quis feugiat. Laoreet leo dolor, dui eget sit viverra justo,
-                            malesuada. Viverra pharetra, augue neque Sit ullamcorper at gravida quis feugiat. Laoreet
-                            leo dolor, dui eget sit viverra justo, malesuada. Viverra pharetra, augue neque felis enim
-                            dui id cum. At pellentesque diam nulla ac amet quisque quis. Est consectetur ullamcorper
-                            curabitur quis viverra hac molestie. Elit pulvinar congue ut amet adipiscing felis
-                            tincidunt. Amet quis varius aliquam hendrerit tempus. Sed sit diam quis
-                        </p>
-                    </div>
-                </div>
-                <div className="news__box">
-                    <img src={newsImg} alt="" className="news__img"/>
-                    <div className="news__about">
-                        <p className="news__subtitle">
-                            Lorem ipsum dolor sit amet
-                        </p>
-                        <p className="news__desc">
-                            Sit ullamcorper at gravida quis feugiat. Laoreet leo dolor, dui eget sit viverra justo,
-                            malesuada. Viverra pharetra, augue neque felis enim dui id cum. At pellentesque diam nulla
-                            ac amet quisque quis. Est consectetur ullamcorper curabitur quis viverra hac molestie. Elit
-                            pulvinar congue ut amet adipiscing felis tincidunt. Amet quis varius aliquam hendrerit
-                            tempus. Sed sit diam quis scelerisque congu econgu econgu econguecongu econguecon guecon
-                            guecon guecong ueconguecong uec ongue.
-                            Sit ullamcorper at gravida quis feugiat. Laoreet leo dolor, dui eget sit viverra justo,
-                            malesuada. Viverra pharetra, augue neque Sit ullamcorper at gravida quis feugiat. Laoreet
-                            leo dolor, dui eget sit viverra justo, malesuada. Viverra pharetra, augue neque felis enim
-                            dui id cum. At pellentesque diam nulla ac amet quisque quis. Est consectetur ullamcorper
-                            curabitur quis viverra hac molestie. Elit pulvinar congue ut amet adipiscing felis
-                            tincidunt. Amet quis varius aliquam hendrerit tempus. Sed sit diam quis
-                        </p>
-                    </div>
-                </div>
+                {
+                   news ?
+                       news?.map((item,index)=>
+                           <div className="news__box" key={index} >
+                               <img src={item.image} alt="" className="news__img"/>
+                               <div className="news__about">
+                                   <p className="news__subtitle">
+                                       {item.title}
+                                   </p>
+                                   <p className="news__desc">
+                                       {item.desc}
+                                   </p>
+                               </div>
+                           </div>
+                       )
+                       : null
+                }
             </div>
         </div>
     );
